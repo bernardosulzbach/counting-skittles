725d3c0fc5ae456b3fc565c32148cfc90faf1be8
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2019-11-04 12:34:55 -0300

    Adds several operators which might be useful

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import numpy
 import os
 import math
-import cv2
 import json
 import datetime
+
+import numpy
+import cv2
+
+REFERENCE_COLORS = {
+    "green": (73, 149, 0),
+    "red": (131, 15, 24),
+    "purple": (49, 28, 25),
+    "yellow": (247, 197, 62),
+    "orange": (234, 75, 16)
+}
 
 
 def average_color(image, c):
@@ -21,7 +30,7 @@ def average_color(image, c):
     if count:
         return [x / count for x in total]
     else:
-        return [0.0 for x in total]
+        return [0.0 for _ in total]
 
 
 def color_distance(a, b):
@@ -54,14 +63,53 @@ def find_circles(color_image):
     return circles[0]
 
 
+def find_and_write_circles(image, filename, destination_path):
+    circles = find_circles(image)
+    print('Found {} circles in {}.'.format(len(circles), filename))
+    cv2.imwrite(destination_path, image)
+
+
 def ensure_path_exists(path):
     os.makedirs(path)
 
 
 class Instance:
-    def __init__(self, id: str, counts: dict):
-        self.id = id
+    def __init__(self, identifier: str, counts: dict):
+        self.id = identifier
         self.counts = counts
+
+
+def saturation_cut(image, destination_path):
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    threshold = numpy.float32(0.4 * 255.0)
+    zero = numpy.float32(0.0 * 255.0)
+    one = numpy.float32(1.0 * 255.0)
+    vectorized = hsv_image.reshape((-1, 3))
+    vectorized[:, 1] = numpy.where(vectorized[:, 1] < threshold, zero, vectorized[:, 1])
+    vectorized[:, 2] = numpy.where(vectorized[:, 1] == zero, one, vectorized[:, 2])
+    hsv_image = vectorized.reshape(hsv_image.shape)
+    hsv_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
+    cv2.imwrite(destination_path, hsv_image)
+
+
+def segment(image, destination_path):
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    threshold = numpy.float32(0.3 * 255.0)
+    zero = numpy.float32(0.0 * 255.0)
+    one = numpy.float32(1.0 * 255.0)
+    vectorized = numpy.float32(hsv_image.reshape((-1, 3)))
+    vectorized[:, 1] = numpy.where(vectorized[:, 1] < threshold, zero, vectorized[:, 1])
+    vectorized[:, 0] = numpy.where(vectorized[:, 1] == zero, zero, vectorized[:, 0])
+    vectorized[:, 2] = numpy.where(vectorized[:, 1] == zero, one, one)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    clusters = 6
+    attempts = 10
+    flags = cv2.KMEANS_PP_CENTERS
+    return_value, best_labels, centers = cv2.kmeans(vectorized, clusters, None, criteria, attempts, flags)
+    centers = numpy.uint8(centers)
+    segmented_image = centers[best_labels.flatten()].reshape(hsv_image.shape)
+    segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_HSV2BGR)
+    cv2.imwrite(destination_path, segmented_image)
 
 
 def main():
@@ -70,15 +118,19 @@ def main():
     date_and_time = datetime.datetime.now().isoformat('-')
     output_path = os.path.join('output', date_and_time)
     ensure_path_exists(output_path)
+    segmented_path = os.path.join('segmented', date_and_time)
+    ensure_path_exists(segmented_path)
+    saturation_path = os.path.join('saturation', date_and_time)
+    ensure_path_exists(saturation_path)
     instances = []
     for label in labels:
         instances.append(Instance(label, labels[label]))
         filename = label + '.jpg'
         image = cv2.imread(os.path.join('data/images', filename))
         image = cv2.resize(image, (image.shape[1] // 4, image.shape[0] // 4))
-        circles = find_circles(image)
-        print('Found {} circles in {}.'.format(len(circles), filename))
-        cv2.imwrite(os.path.join(output_path, filename), image)
+        find_and_write_circles(image.copy(), filename, os.path.join(output_path, filename))
+        segment(image.copy(), os.path.join(segmented_path, filename))
+        saturation_cut(image.copy(), os.path.join(saturation_path, filename))
 
 
 if __name__ == '__main__':
